75695ad0b6897509468aff042d8fcd2b8c88008f
ajc-li <allan.jai.li@gmail.com>
2016-06-30 12:31:08 -0600

    testing conditions

--- c4test/spec/board_spec.rb
+++ c4test/spec/board_spec.rb
@@ -1,9 +1,52 @@
-require 'board.rb'
+require 'lib/board.rb'
 
 describe Board do
   let(:board) {Board.new}
 
-  desbribe '#intialize' do
-    
-	
+  describe '#initialize' do
+  	it 'raises error with no arugments' do 
+  	  expect(board).to raise_error(ArgumentError)
+  	end
+  end
+
+  # describe '#add piece' do
+  # 	it 'adds piece properly' do
+  # 	  expect(board)
+  # 	end
+
+  describe '#validformat' do
+  	it 'accepts valids inputs only' do
+  	  expect(board.column_available?(0..6)).to eq(true)
+  	end
+  end
+
+  # describe '#invalidformat' do
+  # 	it 'rejects invalid formats' do
+  # 	  expect(board.column_available?(=<0)).to eq(false)
+  # 	  expect(board.column_available?(>=6)).to eq(false)
+  # 	end
+
+  describe '#novictory' do
+  	it 'does not win at start of game' do
+  	  expect(board.winning_combination?).to eq(false)
+  	end
+  end
+
+  describe '#winningvertical?' do
+  	it 'wins game when winning combintation vertcal is found' do
+  	  expect(board.winning_vertical?).to eq(true)
+  	end
+  end
+
+  describe '#winninghorizontal?' do
+  	it 'wins games when winning combintation horizontal is found' do
+  	  expect(board.winning_horizontal?).to eq(true)
+  	end
+  end
+
+  describe '#winningdiagonal?' do
+  	it 'wins game when winning combintation diagonal is found' do
+  	  expect(board.winning_diagonal?).to eq(true)
+  	end
+  end	
 end
