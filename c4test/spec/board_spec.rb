634072243824f3ab21d11c99f19b81b00c3aa9da
ajc-li <allan.jai.li@gmail.com>
2016-06-30 18:38:32 -0600

    finished

--- c4test/spec/board_spec.rb
+++ c4test/spec/board_spec.rb
@@ -9,22 +9,11 @@ describe Board do
   	end
   end
 
-  # describe '#add piece' do
-  # 	it 'adds piece properly' do
-  # 	  expect(board)
-  # 	end
-
   describe '#validformat' do
   	it 'accepts valids inputs only' do
   	  expect(board.column_available?(0..6)).to eq(true)
   	end
   end
-
-  # describe '#invalidformat' do
-  # 	it 'rejects invalid formats' do
-  # 	  expect(board.column_available?(=<0)).to eq(false)
-  # 	  expect(board.column_available?(>=6)).to eq(false)
-  # 	end
 
   describe '#novictory' do
   	it 'does not win at start of game' do
@@ -34,18 +23,36 @@ describe Board do
 
   describe '#winningvertical?' do
   	it 'wins game when winning combintation vertcal is found' do
+  	  board.boardx[0][0]="X"
+  	  board.boardx[0][1]="X"
+  	  board.boardx[0][2]="X"
+  	  board.boardx[0][3]="X"
+  	  board.boardx[0][4]="X"
+  	  board.boardx[0][5]="X"
   	  expect(board.winning_vertical?).to eq(true)
   	end
   end
 
   describe '#winninghorizontal?' do
   	it 'wins games when winning combintation horizontal is found' do
+  	  board.boardx[0][0]="X"
+  	  board.boardx[1][0]="X"
+  	  board.boardx[2][0]="X"
+  	  board.boardx[3][0]="X"
+  	  board.boardx[4][0]="X"
+  	  board.boardx[5][0]="X"
   	  expect(board.winning_horizontal?).to eq(true)
   	end
   end
 
   describe '#winningdiagonal?' do
   	it 'wins game when winning combintation diagonal is found' do
+  	  board.boardx[0][0]="X"
+  	  board.boardx[1][1]="X"
+  	  board.boardx[2][2]="X"
+  	  board.boardx[3][3]="X"
+  	  board.boardx[4][4]="X"
+  	  board.boardx[5][5]="X"
   	  expect(board.winning_diagonal?).to eq(true)
   	end
   end	
